d5e3eb15776eda4cb685a0cb924d4c12e1ef929b
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-10 13:05:17 -0700

    Added list

---
+++ data/items.js
@@ -0,0 +1,28 @@
+const potionOfHealing = {
+    id: 'potionOfHealing',
+    name: 'Potion of Healing',
+    image: 'potionofhealing.png',
+    description: 'Taste like cherry!',
+    category: 'potion',
+    price: 5
+};
+
+const flametongue = {
+    id: 'flametongue',
+    name: 'Flametongue',
+    image: 'flametongue.png',
+    description: 'Both hot and sharp!',
+    category: 'weapon',
+    price: 35
+};
+
+const armorOfTheSea = {
+    id: 'armorOfTheSea',
+    name: 'Armor of the Sea',
+    image: 'armorofthesea.png',
+    description: 'Constantly smells of the sea; will leave you damp.',
+    category: 'armor',
+    price: 80
+};
+
+export const items = [potionOfHealing, flametongue, armorOfTheSea];
---
+++ product/functions.js
@@ -0,0 +1,5 @@
+export function renderItem(item) {
+    const li = document.createElement('li');
+    li.classList.add
+    
+}
